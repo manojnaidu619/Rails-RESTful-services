21636054c01582508930fb73a206b7aef0b67c6c
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-01-25 21:51:11 +0530

    solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125091659) do
+ActiveRecord::Schema.define(version: 20170726120007) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "content"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
